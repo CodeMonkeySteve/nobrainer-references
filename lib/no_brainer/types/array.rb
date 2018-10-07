66de45f229df3c95dbef0093e714278bd4f60a6b
Steve Sloan <steve@finagle.org>
2018-10-06 22:44:49 -0700

    Array: proxy NoBrainer serialization methods

--- lib/no_brainer/types/array.rb
+++ lib/no_brainer/types/array.rb
@@ -2,7 +2,19 @@ require 'no_brainer/document'
 
 module NoBrainer
   class Array
-    def self.of(object_type)
+    %i(nobrainer_cast_user_to_model nobrainer_cast_model_to_db nobrainer_cast_db_to_model).each do |method|
+      redefine_singleton_method(method) do |values|
+        ::Array.wrap(values).map do |value|
+          if value.class.respond_to?(method)
+            value.class.__send__(method, value)
+          else
+            value
+          end
+        end
+      end
+    end
+
+    def self.of(object_type = nil)
       NoBrainer::TypedArray.of(object_type)
     end
   end
