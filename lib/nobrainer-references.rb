0ff850c2322a03a0dbe7bf03675c49e3f366da5e
Steve Sloan <steve@finagle.org>
2021-05-09 21:35:19 -0700

    Removed NoBrainer::Array (moved to nobrainer gem).

--- lib/nobrainer-references.rb
+++ lib/nobrainer-references.rb
@@ -1,8 +1,6 @@
 require 'active_support/dependencies/autoload'
 
 require 'nobrainer'
-require 'no_brainer/types/array'
-require 'no_brainer/types/numeric'
 require 'no_brainer/document/references'
 require 'no_brainer/document/embeddable'
 
